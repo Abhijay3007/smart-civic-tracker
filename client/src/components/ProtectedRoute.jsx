bd524fb85da63bb3eed7ecd1d77518ef4d540c20
Abhijay 3007 <abhijayr05@gmail.com>
2026-02-10 21:40:14 +0530

    routing corrections

--- client/src/components/ProtectedRoute.jsx
+++ client/src/components/ProtectedRoute.jsx
@@ -1,6 +1,6 @@
 import { Navigate } from "react-router-dom";
 import { useContext } from "react";
-import { AuthContext } from "../context/authContext";
+import { AuthContext } from "../context/AuthContext";
 
 const ProtectedRoute = ({ children, adminOnly = false }) => {
   const { user } = useContext(AuthContext);
