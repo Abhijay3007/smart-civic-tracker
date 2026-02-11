7ceed45ee5c3bdb76e935af86b746f58bc2ec117
Abhijay 3007 <abhijayr05@gmail.com>
2026-02-11 14:02:56 +0530

    baseURL changes

--- client/src/services/api.js
+++ client/src/services/api.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const api = axios.create({
-  baseURL: "https://smart-civic-tracker-1.onrender.com/api",
+  baseURL: "https://smart-civic-tracker-3.onrender.com/api",
 });
 
 // Attach token to every request
